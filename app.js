3988961025e795976e60d4629bef3cee499ef2df
FiaresAlan <alandelimafiares@hotmail.com>
2025-09-13 17:28:15 -0300

    Exercício 03 - Realizado

--- app.js
+++ app.js
@@ -2,44 +2,37 @@ const express = require('express');
 const app = express();
 const PORT = 8081 || 8080 || 3000;
 
-app.get("/calculadora", (req, res)=>{
-    try {
-        const {operacao, numUm, numDois} = req.query;
-        
-        if(isNaN(numUm) || numUm == undefined || numUm == null || isNaN(numDois) || numDois == undefined || numDois == null){
-            return res.status(400).send(`Entrada de número inválido!`);
-        }
-        const numberUm = parseFloat(numUm);
-        const numberDois = parseFloat(numDois);
+app.get('/operacao/:tipo', (req, res)=>{
+    const { numUm, numDois } = req.query; //soma, multiplicação, subtração, divisão 
+    const { tipo } = req.params; //números
+
+    if(isNaN(numUm) || numUm == undefined || numUm == null || isNaN(numDois) || numDois == undefined || numDois == null){
+        return res.status(400).send(`Entrada de número inválido!`);}
+
+        const numUmFloat = parseFloat(numUm);
+        const numDoisFloat = parseFloat(numDois);
         let resultado;
-        switch (operacao) {
-            case "soma":
-                resultado = (numberUm + numberDois).toFixed(2);
-                break;
-            
-            case "subtracao":
-                resultado = (numberUm - numberDois).toFixed(2);
-                break;
-            
-            case "multiplicacao":
-                resultado = (numberUm * numberDois).toFixed(2);
-                break;
-            
-            case "divisao":
+    switch(tipo){
+        case 'soma':
+            resultado = (numUmFloat + numDoisFloat).toFixed(2);
+            break;
+        case 'subtracao':
+            resultado = (numUmFloat - numDoisFloat).toFixed(2);
+            break;
+        case 'multiplicacao':
+            resultado = (numUmFloat * numDoisFloat).toFixed(2);
+            break;
+        case 'divisao':
                 if (numberDois === 0 || numberUm === 0){
                     return res.status(400).send(`Não divida por zero`, error)
                 }
                 resultado = (numberUm / numDois).toFixed(2);
                 break;
-            default:
-                return res.status(400).send("Erro ocorreu:", error)
+        default:
+            return res.status(400).json({ error: "Tipo de operação inválida. Use 'soma', 'subtração', 'multiplicação' ou 'divisão'." });
         }
-    res.send(`O resultado da operação é ${numberUm} e ${numDois} = ${resultado}`)
-    }
-    catch (error) {
-        return console.error(`Erro reportado `, error)
-    }
-})
+    res.status(200).send(`O resultado da ${tipo} entre ${numUmFloat} e ${numDoisFloat} é ${resultado}`);
+});
 
 app.listen(PORT, ()=>{
     console.log(`Servidor esta sendo executado na porta http://localhost:${PORT}`)
--- task.md
+++ task.md
@@ -1,6 +1,5 @@
-2. Crie uma rota /calculadora que receba os parâmetros via query:
-operacao (valores possíveis: soma, subtracao, multiplicacao, divisao);
-numUm e numDois (números para operação);
-A rota deve processar os parâmetros e retornar o resultado da operação solicitada.
+3. Crie uma rota /operacao/:tipo onde tipo é a operação matemática (soma, subtracao, multiplicacao, divisao).
+Os números 
 
-GET /calculadora?operacao=soma&numUm=4&numDois=6 → Retorna Resultado: 10
+numUm e numDois serão passados via query parameters.
+A rota deve executar a operação e retornar o resultado.  
