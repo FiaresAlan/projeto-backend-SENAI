52da7bd18a64f5b5df20b85e610648a7badf0f65
unknown <alandelimafiares@HOTMAO>
2025-09-20 17:11:14 -0300

    Tasks 1-3: Realizadas

--- app.js
+++ app.js
@@ -12,19 +12,22 @@
 // responda status 200 com o resultado filtrado.
 // se ocorrer erro ao ler o arquivo, devolva status 500 no catch.
 
-
+// 3. Crie um arquivo eventos.json com eventos contendo campos id, nome, data.
+// Crie a rota GET /eventos que aceite um query parameter data para filtrar eventos em determinada data:
+// Exemplo: GET /eventos?data=2025-08-10.
+// responda status 200 com os eventos daquela data, ou 500 se ocorrer erro no try/catch.
 
 const express = require('express');
 const app = express();
 const PORT = 8080 || 8081 || 3031;
 const fs = require("fs");
 
+//Task 01
 app.get("/usuarios", (req, res)=>{
     try {
         const data = fs.readFileSync("./usuarios.json", "utf-8");
-
         let usuarios = JSON.parse(data)
-
+        //Task 02
         const {nomeUser} = req.query;
 
         if (nomeUser) {
@@ -34,11 +37,28 @@ app.get("/usuarios", (req, res)=>{
 
 
     } catch (error) {
-        console.error(`Erro ao ler o arquivo JSON ${error}`);
+        console.error(`Erro ao ler o arquivo usuarios.JSON ${error}`);
         res.status(500).json({message: "Erro interno no servidor POST 500."})
     }
 })
+//Task 03
+app.get("/eventos", (req, res)=>{
+    try {
+        const dado = fs.readFileSync("./eventos.json", "utf-8");
+        let eventos = JSON.parse(dado);
+        const {dataEvento} = req.query;
 
+        if (dataEvento) {
+            eventos = eventos.filter(evento => evento.data <= dataEvento)
+        }
+        
+        res.status(200).json(eventos)
+        
+    } catch (error) {        
+        console.error(`Erro ao ler o arquivo eventos.JSON ${error}`);
+        res.status(500).json({message: "Erro interno no servidor POST 500."})     
+    }
+})
 
 
 
